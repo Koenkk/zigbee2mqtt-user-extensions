a547d817d24b331a9842ba04a4fc7e6e4451a178
Jorge Schrauwen <registration@blackdot.be>
2023-05-24 17:25:39 +0200

    Add color mode reader extension (#1)
    * Add color_mode_reader.js
    
    This adds a little helper extension to trigger a read of colorMode when any of the color related attributes get reported for a Signify bulb.
    
    * Move color_mode_reader.js to a directory
    
    * Update the main README.md and add a README.md for color_mode_reader
    
    ---------
    
    Co-authored-by: Jorge Schrauwen <jorge@blackdot.be>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Zigbee2MQTT user extensions
 This repository contains user extensions to be used with Zigbee2MQTT. For more information see the [Zigbee2MQTT docs](https://www.zigbee2mqtt.io/advanced/more/user_extensions.html).
+
+## Stable
+The extensions listed here are considered **stable** and are not expected to  get breaking changes or a lot of active development.
+
+- [color_mode_reader](stable/color_mode_reader/README.md): simple extension to read the read `lightingColorCtrl.colorMode` when reporting is not supported.
+
+## Unstable
+The extensions listed here are considered **unstable** and are under active development, breaking changes and frequent updates are expected.
+
+- Looks like there are no unstable extensions at the momnent.
---
+++ stable/color_mode_reader/color_mode_reader.js
@@ -0,0 +1,84 @@
+/*
+ * The newer Philips Hue with BT bulbs (by signify) support reporting, except for color_mode
+ *
+ * This extension will read the colorMode attribute if any of the known color attributes get reported.
+ */
+class ColorModeReader {
+    constructor(zigbee, mqtt, state, publishEntityState, eventBus, settings, logger) {
+        this.zigbee = zigbee;
+        this.mqtt = mqtt;
+        this.state = state;
+        this.publishEntityState = publishEntityState;
+        this.eventBus = eventBus;
+        this.settings = settings;
+        this.logger = logger;
+        this.colorAttributes = [
+            'colorTemperature', 'currentX', 'currentY',
+            'currentHue', 'enhancedCurrentHue',
+            'currentSaturation',
+        ];
+    }
+
+    async start() {
+        this.logger.info('Starting color_mode helper');
+
+        // attach to events
+        this.eventBus.on('deviceMessage', this.onDeviceMessage.bind(this), this.constructor.name);
+    }
+
+    async stop() {
+        this.eventBus.removeListeners(this.constructor.name);
+    }
+
+    onDeviceMessage(data) {
+        // skip non interviewed devices
+        if (!data.device.zh.interviewCompleted) return;
+
+        // filter reporting of lightingColorCtrl
+        if (
+            (data.type == 'attributeReport') &&
+            (data.cluster == 'lightingColorCtrl')
+        ) {
+            // filter on color related attributes
+            for (const attrib of Object.keys(data.data)) {
+                if (this.colorAttributes.includes(attrib)) {
+                    // check if device has colorMode reporting
+                    if (!this.hasColorModeReporting(data.device)) {
+                        this.readColorMode(data.device, data.endpoint, Object.keys(data.data))
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    /**
+     * Look for colorMode or enhancedColorMode reporting
+     */
+    hasColorModeReporting(device) {
+        let ret = false;
+        device.zh.endpoints.forEach(ep => {
+            ep.configuredReportings.forEach(report => {
+                // skip reports on non lightingColorCtrl cluster
+                if (report.cluster.name != "lightingColorCtrl") return;
+
+                // skip reports on non colorMode attributes
+                if (!['colorMode', 'enhancedColorMode'].includes(report.attribute.name)) return;
+
+                ret = true;
+            });
+        });
+        return ret;
+    }
+
+    /**
+     * Read colorMode and original attributes from report
+     */
+    readColorMode(device, endpoint, attributes=[]) {
+        attributes.push('colorMode');
+        this.logger.debug(`Reading '${attributes.join(', ')}' from '${device.name}' (${device.ieeeAddr})`);
+        endpoint.read('lightingColorCtrl', attributes);
+    }
+}
+
+module.exports = ColorModeReader;
